44a0874baff5c7ae34b8575b09bc842e011bd5b3
rickykrissel <rickyk06@outlook.com>
2026-04-08 11:04:16 -0400

    implemented lookahead for get_best_move and added board_to_np and simulate placement functions

--- genetic.py
+++ genetic.py
@@ -2,6 +2,8 @@ import numpy as np
 from copy import copy, deepcopy
 import random
 from genetic_helpers import *
+from board import Board
+from piece import BODIES2, Piece
 
 
 class Genetic_AI:
@@ -60,32 +62,64 @@ class Genetic_AI:
 
         return aggregate_rating
 
-
-    def get_best_move(self, board, piece):
+    def simulate_placement(self, board, piece, x):
         """
-        Gets the best for move an agents base on board, next piece, and genotype
+        returns a new board with piece placed at x dropping to y with rows cleared or 
+        none if invalid placement
+        """
+        y = board.drop_height(piece,x)
+
+        sim = Board()
+        sim.board = [row[:] for row in board.board]
+        sim.widths = board.widths[:]
+        sim.heights = board.heights[:]
+        sim.colors = board.colors
+
+        result = sim.place(x,y, piece)
+
+        if isinstance(result, Exception):
+            return None
+        sim.clear_rows()
+        return sim
+
+     def _board_to_np(self, sim_board):
+        """Convert Board object to numpy array for valuate()."""
+        f = lambda cell: 1 if cell else 0
+        return np.array([[f(cell) for cell in row] for row in sim_board.board])
+
+
+    def get_best_move(self, board, piece, lookahead=True):
+        """
+        Gets the best for move an agents based on board, next piece, and genotype
+
+        With lookahead=True: for each piece-1 placement, evaluate the best
+        possible piece-2 outcome and use that as the score.
+        With lookahead=False: original depth-1 behaviour.
         """
 
-        best_x = -1000
-        max_value = -1000
+        best_x = -1
+        best_score = -float('inf')
         best_piece = None
         for i in range(4):
             piece = piece.get_next_rotation()
-            for x in range(board.width):
+            for x in range(board.width-len(piece.skirt)+1):
+                #place piece 1
                 try:
-                    y = board.drop_height(piece, x)
+                    sim1 = simulate_placement(board, piece, x)
                 except:
                     continue
+                if sim1 is None:
+                    continue
 
-                board_copy = deepcopy(board.board)
-                for pos in piece.body:
-                    board_copy[y + pos[1]][x + pos[0]] = True
-
-                np_board = bool_to_np(board_copy)
-                c = self.valuate(np_board)
-
-                if c > max_value:
-                    max_value = c
+                if not lookahead:
+                    score = self.valuate(self._board_to_np(sim1))
+                else:
+                    score = self._lookahead_score(sim1)
+                if score > best_score:
+                    best_score = score
                     best_x = x
                     best_piece = piece
+        
         return best_x, best_piece
+
+#implement functions after this (lookahead_score, etc.)
